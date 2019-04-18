1f56a25c0d102df959a86490b55c4432e3137c8a
Tripti Singh <stripti411@gmail.com>
2019-04-18 11:33:47 +0530

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,8 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.linear_model import LinearRegression
-import statsmodels.formula.api as sm
-
+from statsmodels.formula.api import OLS
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
@@ -38,7 +37,15 @@ y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
 
-X = np.append(arr=np.ones((50, 1), dtype=int, values=X, axis=1))
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
+regressor_OLS = OLS(endog=y, exog=X_opt).fit()
+X_opt = X[:, [0, 1, 3, 4]]
+regressor_OLS = OLS(endog=y, exog=X_opt).fit()
+X_opt = X[:, [0, 1, 3]]
+regressor_OLS = OLS(endog=y, exog=X_opt).fit()
+X_opt = X[:, [0, 1]]
+regressor_OLS = OLS(endog=y, exog=X_opt).fit()
+X_opt = X[:, [0, 1]]
+regressor_OLS = OLS(endog=y, exog=X_opt).fit()
+print(regressor_OLS.summary())

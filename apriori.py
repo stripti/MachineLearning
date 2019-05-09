f0e70b575f5439148a74a76332bc963f00ff3934
Tripti Singh <stripti411@gmail.com>
2019-05-09 11:12:36 +0530

    Association Rule Learning (Method 1-Apriori)

---
+++ apriori.py
@@ -0,0 +1,19 @@
+# Apriori
+
+# Importing the libraries
+from apyori import apriori
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
+
+rules = apriori(transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
+
+# Visualising the results
+results = list(rules)
+print(results)

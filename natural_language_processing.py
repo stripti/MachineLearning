a7e45a96a640ff8cbd225e92482b8973fb14e6a6
Tripti Singh <stripti411@gmail.com>
2019-05-16 16:17:04 +0530

    Update natural_language_processing.py (used Naive Bayes classifier)

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -25,5 +25,30 @@ for i in range(0, 1000):
     review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
-#print(corpus)
+# print(corpus)
 # Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)

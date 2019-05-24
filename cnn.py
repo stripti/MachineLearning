ef20776a716ae534447b4159ed65afc5d8926423
Tripti Singh <stripti411@gmail.com>
2019-05-24 20:39:02 +0530

    CNN

---
+++ cnn.py
@@ -0,0 +1,74 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution(adding a convolution layer to the network)
+# Here, we take 32 as nb_filter because we are working on a CPU and not on GPU so it will take time to process..and no of rows and col of feature detector as 3 and 3...input_shape helps us to change all images to a particular size here we take as 64*64 and 3 as there are 3 dimension array as colors are taken into acccount(RGB)
+
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+# we choose mostly 2*2 as the size of pool because we dont wanna loose the information and still be precise about the max value in the map that corresponds to the feature matched
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+# in this step we take output_dim i.e the no of hidden layers as a choice by experiments as 128 ..its a no not too small to make classifier good model not too big to make it highly compute intensive..so keep it near 100 but a power of 2
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
--- data_processing_template.py
+++ data_processing_template.py
@@ -9,13 +9,13 @@ import pandas as pd
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.purchased.values
+y = dataset.iloc[:, :3].values
 
 # y=dataset.purchased also works..
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
